b79cfa4ff618866a36e0514b4def6c538fd36849
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2024-11-10 16:15:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,14 +16,44 @@ app.get('/', (req, res) => {
   res.redirect('/index');
 });
 
-app.get('/Dorm', (req, res) => {
+app.get('/dormitory', (req, res) => {
     res.sendFile(path.join(__dirname, 'Front', 'dormitory.html'));
   });
-  
-app.get('/Front/Dorm/dorm123.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dormitory.html'));
+});
+
+app.get('/dorm123', (req, res) => {
     res.sendFile(path.join(__dirname, 'Front', 'dorm123.html'));
   });
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123.html'));
+});
+app.get('/dorm123location', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123_location.html'));
+});
+app.get('/dorm123public', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123_public.html'));
+});
+app.get('/dorm123fee2people', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee2people.html'));
+});
+app.get('/dorm123fee4people', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee4people.html'));
+});
+app.get('/dorm123QA', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dorm123QA.html'));
+});
+app.get('/dormtku', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dormtku.html'));
+});
+app.get('/dormtkulocation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dormtku_location.html'));
+});
+app.get('/dormtkufee', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dormtku_fee.html'));
+});
+app.get('/dormtkupublic', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Front', 'dormtku_public.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
-});
