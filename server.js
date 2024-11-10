af1dae1c48156dc9f754dcbf4af6b2984508fe6e
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2024-11-10 16:16:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,57 +3,60 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
 app.use(express.static(path.join(__dirname, 'Front')));
 
-
 app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'index.html'));
 });
 
-
 app.get('/', (req, res) => {
-  res.redirect('/index');
+    res.redirect('/index');
 });
 
 app.get('/dormitory', (req, res) => {
     res.sendFile(path.join(__dirname, 'Front', 'dormitory.html'));
-  });
-  res.sendFile(path.join(__dirname, 'Front', 'dormitory.html'));
 });
 
 app.get('/dorm123', (req, res) => {
     res.sendFile(path.join(__dirname, 'Front', 'dorm123.html'));
-  });
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123.html'));
 });
+
 app.get('/dorm123location', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123_location.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dorm123_location.html'));
 });
+
 app.get('/dorm123public', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123_public.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dorm123_public.html'));
 });
+
 app.get('/dorm123fee2people', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee2people.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee2people.html'));
 });
+
 app.get('/dorm123fee4people', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee4people.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dorm123_fee4people.html'));
 });
+
 app.get('/dorm123QA', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dorm123QA.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dorm123QA.html'));
 });
+
 app.get('/dormtku', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dormtku.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dormtku.html'));
 });
+
 app.get('/dormtkulocation', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dormtku_location.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dormtku_location.html'));
 });
+
 app.get('/dormtkufee', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dormtku_fee.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dormtku_fee.html'));
 });
+
 app.get('/dormtkupublic', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Front', 'dormtku_public.html'));
+    res.sendFile(path.join(__dirname, 'Front', 'dormtku_public.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+});
